2992c1d81304842f36b4a5cb1223ca42629f9b20
sunguo <582447281@qq.com>
2021-06-09 14:30:51 +0800

    【功能问题】：优化 【提交说明】：优化 【提交人】：孙国

--- src/views/modules/WebGL-guide-chapter02.vue
+++ src/views/modules/WebGL-guide-chapter02.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <h3>鼠标点击绘制点</h3>
     <div>
-      <canvas id="webgl" width="400" height="400"></canvas>
+      <canvas id="webgl" width="400" height="400" @click="clickOpt"></canvas>
     </div>
   </div>
 </template>
@@ -19,23 +20,45 @@ export default {
       const canvas = document.getElementById('webgl')
       const gl = canvas.getContext('webgl')
       this.initShader(gl)
+      this.gl = gl
     },
     initShader(gl) {
-      const vertexShader = `
-      void main(){
-        gl_Position = vec4(0.5, 0.5, 0.0, 1.0);
+      const vertexSahder = `
+      attribute vec4 a_Position;
+      void main() {
+        gl_Position = a_Position;
         gl_PointSize = 10.0;
       }
       `
       const fragmentShader = `
-      void main(){
-        gl_FragColor=vec4(0.5,0.2,0.5,1.0);
+      precision mediump float;
+      uniform vec4 u_FragColor;
+      void main() {
+        gl_FragColor = u_FragColor;
       }
       `
-      initShaders(gl, vertexShader, fragmentShader)
+      initShaders(gl, vertexSahder, fragmentShader)
+
+      this.a_Position = gl.getAttribLocation(gl.program, 'a_Position')
+      this.u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor')
+
       gl.clearColor(0.0, 0.0, 0.0, 1.0)
       gl.clear(gl.COLOR_BUFFER_BIT)
-      gl.drawArrays(gl.POINTS, 0, 2)
+    },
+    clickOpt(event) {
+      const gl = this.gl
+      let target = event.target
+      let x = event.clientX - target.offsetLeft
+      let y = event.clientY - target.offsetTop
+      x = (x - target.width / 2) / (target.width / 2)
+      y = (target.height / 2 - y) / (target.height / 2)
+      const colors = new Array(4)
+        .fill(0)
+        .map(() => Number(Math.random().toFixed(2)))
+      gl.vertexAttrib3f(this.a_Position, x, y, 0.0)
+      gl.uniform4f(this.u_FragColor, colors[0], colors[1], colors[2], colors[3])
+      gl.clear(gl.COLOR_BUFFER_BIT)
+      gl.drawArrays(gl.POINTS, 0, 1)
     }
   }
 }
